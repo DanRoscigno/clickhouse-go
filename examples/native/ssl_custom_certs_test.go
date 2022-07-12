522c3b89eac35fa324cffa67167fc750c8ee95c7
Dale Mcdiarmid <dale@clickhouse.com>
2022-07-12 17:30:13 +0100

    custom CA cert

--- examples/native/ssl_custom_certs_test.go
+++ examples/native/ssl_custom_certs_test.go
@@ -2,23 +2,33 @@ package examples
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/stretchr/testify/require"
+	"io/ioutil"
 	"testing"
 )
 
 func sslCustomCertsVersion() (string, error) {
-	var (
-		conn, err = clickhouse.Open(&clickhouse.Options{
-			Addr: []string{"play.clickhouse.com:9440"},
-			TLS: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-			Auth: clickhouse.Auth{
-				Username: "explorer",
-			},
-		})
-	)
+	t := &tls.Config{}
+	caCert, err := ioutil.ReadFile("play.clickhouse.pem")
+	if err != nil {
+		return "", err
+	}
+	caCertPool := x509.NewCertPool()
+	successful := caCertPool.AppendCertsFromPEM(caCert)
+	if !successful {
+		return "", err
+	}
+	t.RootCAs = caCertPool
+
+	conn, err := clickhouse.Open(&clickhouse.Options{
+		Addr: []string{"play.clickhouse.com:9440"},
+		TLS:  t,
+		Auth: clickhouse.Auth{
+			Username: "explorer",
+		},
+	})
 	if err != nil {
 		return "", err
 	}
